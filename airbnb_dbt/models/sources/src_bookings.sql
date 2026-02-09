a7b409c9507efa91c90f4fb69320dc1812044743
acer <carmenjssu@gmail.com>
2026-02-09 15:04:28 +0000

    socalled bronze layer

--- airbnb_dbt/models/sources/src_bookings.sql
+++ airbnb_dbt/models/sources/src_bookings.sql
@@ -1,1 +1,10 @@
+{{ config(materialization = 'incremental') }}
+
+{% set incremental_key = 'CREATED_AT' %}
+
+
 SELECT * FROM {{ source('staging', 'src_bookings') }}
+
+{% if is_incremental() %}
+    WHERE {{ incremental_key }} > (SELECT coalesce(MAX({{ incremental_key }}), '1900-01-01') FROM {{ this }})
+{% endif %}
